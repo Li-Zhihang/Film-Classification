8334ecee753990c498405a39e3f562f5b9461bab
lizh <lizhihang26@yahoo.com>
2020-05-13 17:21:09 +0800

    fix BGR output bug

--- methods/color/color.py
+++ methods/color/color.py
@@ -6,24 +6,25 @@ except Exception:
 import numpy as np
 
 
-def show_plate(img, cluster_num, color_list):
+def show_plate(img, name, cluster_num, color_list):
     square_len = 50
 
-    def form_square(hsv_color):
-        return np.tile(hsv_color[np.newaxis, np.newaxis, :], (square_len, square_len, 1))
+    def form_square(color):
+        return np.tile(color[np.newaxis, np.newaxis, :], (square_len, square_len, 1))
 
     color_plate = np.zeros((square_len * cluster_num, square_len, 3), dtype=np.uint8)
     for ci in range(cluster_num):
         color_plate[square_len * ci: square_len * (ci + 1)] = form_square(color_list[ci])
 
     print('RGB of center colors:')
+    print(color_list.shape)
     for ci in range(cluster_num):
-        print(color_list[ci, 0], color_list[ci, 1], color_list[ci, 2], sep=', ')
+        print(color_list[ci, 2], color_list[ci, 1], color_list[ci, 0], sep=', ')
 
-    cv.imwrite('dominent_colors2.jpg', color_plate)
+    cv.imwrite(name + '.png', color_plate)
 
 
-def get_dominent_colors(img, cluster_num=5, cluster='kmeans', if_show=False):
+def get_dominent_colors(img, cluster_num=5, cluster='kmeans', if_show=False, name=None):
     pixels = img.reshape(-1, 3).astype(np.float32)
 
     if cluster == 'kmeans':
@@ -40,9 +41,9 @@ def get_dominent_colors(img, cluster_num=5, cluster='kmeans', if_show=False):
 
     color_list = np.zeros((cluster_num, 3), dtype='int')
     for c in range(cluster_num):
-        color_list[c] = centers[c].astype('int')
+        color_list[c] = centers[c, ::-1].astype('int')
 
     if if_show:
-        show_plate(img, cluster_num, color_list)
+        show_plate(img, name, cluster_num, color_list)
 
     return color_list
